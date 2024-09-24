cf37a3d1e10de6d36e0318c6e0a39de43e0ebb9a
Kaïs Dilmi <146418888+Iska1108@users.noreply.github.com>
2024-09-24 10:17:28 +0200

    Crreate-component

--- pmove-client/src/index.js
+++ pmove-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/theme.css";
+import "../src/assets/styles/theme.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
